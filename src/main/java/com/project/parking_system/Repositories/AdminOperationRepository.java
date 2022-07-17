64a59dca1a6d2290881d50d5d0733b2e38bf1329
zarasuto <zarasuto@gmail.com>
2022-07-17 20:52:25 +0800

    Added Delete Employee

--- src/main/java/com/project/parking_system/Repositories/AdminOperationRepository.java
+++ src/main/java/com/project/parking_system/Repositories/AdminOperationRepository.java
@@ -51,4 +51,47 @@ public class AdminOperationRepository {
             return new ResponseDTO("Server Error",false);
         }
     }
+
+    public ResponseDTO DeleteEmployee(EmployeeDTO employeeDTO,String token){
+        url = url+"api/admin/DeleteEmployee";
+
+        //Set Normal headers
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        //Add Authentication here
+        httpHeaders.set("Authorization",token);
+
+        //Set Request Body
+        Gson gson = new Gson();
+        String json = gson.toJson(employeeDTO);
+
+        //Compact it into entity
+        HttpEntity<String> entity = new HttpEntity<>(json,httpHeaders);
+
+        //Post request
+        try{
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(url,entity,String.class);
+            if(responseEntity.getStatusCode() == HttpStatus.OK){
+                ResponseDTO responseDTO = new ResponseDTO(responseEntity.getBody(),true);
+                return responseDTO;
+            }else if(responseEntity.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY){
+                ResponseDTO responseDTO = new ResponseDTO(responseEntity.getBody(),false);
+                return responseDTO;
+            }else{
+                ResponseDTO responseDTO = new ResponseDTO("Deletion failed for unknown cause",false);
+                return responseDTO;
+            }
+        }catch(HttpClientErrorException ex){
+            if(ex.getStatusCode()==HttpStatus.FORBIDDEN){
+                return new ResponseDTO("Failed Authentication",false);
+            }else{
+                return new ResponseDTO(ex.getStatusCode().toString(),false);
+            }
+        }catch (HttpServerErrorException ex){
+            return new ResponseDTO("Server Error",false);
+        }
+    }
 }
