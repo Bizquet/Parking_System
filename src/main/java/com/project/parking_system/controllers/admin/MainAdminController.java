6cdc8ca6350ceea0e2cd05f4d12eeb3f6df9e24b
Vincent Raphael Marcelino <dc_boyyet@outlook.com>
2022-07-18 07:38:44 +0800

    Added delete account functionality. Also added ability to refresh table during reach API transaction.

--- src/main/java/com/project/parking_system/controllers/admin/MainAdminController.java
+++ src/main/java/com/project/parking_system/controllers/admin/MainAdminController.java
@@ -1,10 +1,16 @@
 package com.project.parking_system.controllers.admin;
 
 import com.project.parking_system.Main;
+import com.project.parking_system.Repositories.AdminOperationRepository;
 import com.project.parking_system.Repositories.AuthenticationRepository;
 import com.project.parking_system.controllers.View;
+import com.project.parking_system.datamodel.EmployeeDTO;
+import com.project.parking_system.datamodel.EmployeeDTOFX;
 import com.project.parking_system.datamodel.EmployeeRegistrationDTO;
 import com.project.parking_system.datamodel.LoginDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,10 +18,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class MainAdminController {
@@ -24,20 +33,28 @@ public class MainAdminController {
     private BorderPane mainAdminPane;
 
     @FXML
-//    private TableView<Teller> tellerTable;
+    private TableView tellerTable;
 
     private LoginDTO currentLogin;
     private AuthenticationRepository auth;
+    private AdminOperationRepository adminOp;
 
     public void initData(LoginDTO currentLogin){
         this.currentLogin = currentLogin;
         this.auth = new AuthenticationRepository();
+        this.adminOp = new AdminOperationRepository();
+
+        refreshTable();
+
     }
 
+    private void refreshTable(){
+        Task<ObservableList<EmployeeDTOFX>> task = new GetAllEmployeesTask(currentLogin.getLogin_token());
+        tellerTable.itemsProperty().bind(task.valueProperty());
 
-    public void initialize(){
-        // call task to populate table here
+        new Thread(task).start();
     }
+
 
     @FXML
     public void showAddAccountDialog(){
@@ -79,32 +96,38 @@ public class MainAdminController {
 
             EmployeeRegistrationDTO newEmployee = controller.getNewEmployeeDetails();
             auth.RegisterEmployee(newEmployee, currentLogin.getLogin_token());
+            refreshTable();
 
         }
     }
 
     @FXML
     public void showDeleteAccountDialog(){
-//        Example of getting selection from table
-//        Contact selectedContact = contactsTable.getSelectionModel().getSelectedItem();
-//
-//        if(selectedContact == null){
-//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//            alert.setTitle("No Teller Selected");
-//            alert.setHeaderText(null);
-//            alert.setContentText("Please select teller you want to delete");
-//            alert.showAndWait();
-//            return;
-//        }
+
+
+        EmployeeDTOFX selectedEmployeeFX = (EmployeeDTOFX) tellerTable.getSelectionModel().getSelectedItem();
+
+        if(selectedEmployeeFX == null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No Teller Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select teller you want to delete");
+            alert.showAndWait();
+            return;
+        }
+
+        EmployeeDTO selectedEmployee = new EmployeeDTO(selectedEmployeeFX.getId(), selectedEmployeeFX.getRole(),
+                selectedEmployeeFX.getUsername());
 
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Delete Teller");
-//        alert.setContentText("Are yous sure you want to delete the selected contact? " +
-//                selectedContact.getFirstName() + " " + selectedContact.getLastName());
+        alert.setContentText("Are yous sure you want to delete the selected contact? " +
+                selectedEmployee.getUsername() + " " + selectedEmployee.getRole());
 
         Optional<ButtonType> result = alert.showAndWait();
         if(result.isPresent() && result.get() == ButtonType.OK){
-            // perform deletion
+            adminOp.DeleteEmployee(selectedEmployee, currentLogin.getLogin_token());
+            refreshTable();
         }
     }
 
@@ -139,9 +162,9 @@ public class MainAdminController {
 
         Optional<ButtonType> result = dialog.showAndWait();
 
-        if(result.isPresent() && result.get() == ButtonType.OK){
-            // call controller and change password
-        }
+//        if(result.isPresent() && result.get() == ButtonType.OK){
+//            // call controller and change password
+//        }
 
     }
 
@@ -168,3 +191,30 @@ public class MainAdminController {
 }
 
 // create task to populate table
+class GetAllEmployeesTask extends Task{
+    protected final AdminOperationRepository adminOperation = new AdminOperationRepository();
+    protected String token;
+
+    GetAllEmployeesTask(String token){
+        this.token = token;
+    }
+
+    @Override
+    protected ObservableList<EmployeeDTOFX> call() throws Exception {
+
+        List<EmployeeDTO> employeeDTOList = new ArrayList<>((List<EmployeeDTO>)
+                adminOperation.getAllEmployees(token).getPayload());
+
+        List<EmployeeDTOFX> employeeDTOFXList = new ArrayList<>();
+
+        for(EmployeeDTO employee: employeeDTOList){
+            EmployeeDTOFX employeeDTOFX = new EmployeeDTOFX();
+            employeeDTOFX.setId(employee.getId());
+            employeeDTOFX.setRole(employee.getRole());
+            employeeDTOFX.setUsername(employee.getUsername());
+            employeeDTOFXList.add(employeeDTOFX);
+        }
+
+        return FXCollections.observableArrayList(employeeDTOFXList);
+    }
+}
