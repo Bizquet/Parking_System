00ddf1b66d3bc2e59ab80dda6e8fe8c19ed32b51
zarasuto <zarasuto@gmail.com>
2022-07-17 20:59:30 +0800

    Added Employee Change Password

--- src/main/java/com/project/parking_system/Repositories/AdminOperationRepository.java
+++ src/main/java/com/project/parking_system/Repositories/AdminOperationRepository.java
@@ -2,6 +2,7 @@ package com.project.parking_system.Repositories;
 
 import com.google.gson.Gson;
 import com.project.parking_system.datamodel.EmployeeDTO;
+import com.project.parking_system.datamodel.EmployeeRegistrationDTO;
 import com.project.parking_system.datamodel.ResponseDTO;
 import org.springframework.http.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -82,6 +83,50 @@ public class AdminOperationRepository {
                 return responseDTO;
             }else{
                 ResponseDTO responseDTO = new ResponseDTO("Deletion failed for unknown cause",false);
+                return responseDTO;
+            }
+        }catch(HttpClientErrorException ex){
+            if(ex.getStatusCode()==HttpStatus.FORBIDDEN){
+                return new ResponseDTO("Failed Authentication",false);
+            }else{
+                return new ResponseDTO(ex.getStatusCode().toString(),false);
+            }
+        }catch (HttpServerErrorException ex){
+            return new ResponseDTO("Server Error",false);
+        }
+    }
+
+    public ResponseDTO ChangeEmployeePassword(EmployeeRegistrationDTO employeeDTO, String token){
+        url = url+"api/admin/ChangePassword";
+
+        //Set Normal headers
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        //Add Authentication here
+        httpHeaders.set("Authorization",token);
+
+        //Set Request Body
+        Gson gson = new Gson();
+        String json = gson.toJson(employeeDTO);
+
+        //Compact it into entity
+        HttpEntity<String> entity = new HttpEntity<>(json,httpHeaders);
+
+        //Post request
+        try{
+            ResponseEntity<String> responseEntity = restTemplate.postForEntity(url,entity,String.class);
+
+            if(responseEntity.getStatusCode() == HttpStatus.OK){
+                ResponseDTO responseDTO = new ResponseDTO(responseEntity.getBody(),true);
+                return responseDTO;
+            }else if(responseEntity.getStatusCode() == HttpStatus.UNPROCESSABLE_ENTITY){
+                ResponseDTO responseDTO = new ResponseDTO(responseEntity.getBody(),false);
+                return responseDTO;
+            }else{
+                ResponseDTO responseDTO = new ResponseDTO("Update failed for unknown cause",false);
                 return responseDTO;
             }
         }catch(HttpClientErrorException ex){
